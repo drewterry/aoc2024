8c96b3f48e7872d3b20864f4b2f7cefec6aaea0f
Drew Terry <dterry@relativityspace.com>
2025-02-19 23:28:21 -0500

    day05p2

---
+++ day05p2/solution.go
@@ -0,0 +1,115 @@
+package day05p2
+
+import (
+	"io"
+	"strconv"
+	"strings"
+
+	"aoc/utils"
+)
+
+func Solve(r io.Reader) any {
+	lines := utils.ReadLines(r)
+	_ = lines
+
+	rules := make(map[int]map[int]struct{})
+	prints := make([][]int, 0, 256)
+	section := false
+
+	for _, line := range lines {
+		if line == "" {
+			section = true
+			continue
+		}
+
+		if section {
+
+			pages := strings.Split(line, ",")
+			print := make([]int, 0, len(pages))
+
+			for _, page := range pages {
+				pageInt, err := strconv.Atoi(page)
+				utils.Check(err, "print order: %s", page)
+
+				print = append(print, pageInt)
+			}
+
+			prints = append(prints, print)
+
+		} else {
+			pages := strings.Split(line, "|")
+
+			page1, err := strconv.Atoi(pages[0])
+			utils.Check(err, "rules")
+			page2, err := strconv.Atoi(pages[1])
+			utils.Check(err, "rules")
+
+			rule, exists := rules[page1]
+			if !exists {
+				rule = make(map[int]struct{})
+				rules[page1] = rule
+			}
+
+			rule[page2] = struct{}{}
+		}
+	}
+
+	var result int
+	for _, print := range prints {
+
+		correct := true
+		for i, page := range print {
+			if i == 0 {
+				continue
+			}
+
+			rule, exists := rules[page]
+			if !exists {
+				continue
+			}
+
+			for _, prevPage := range print[:i] {
+				_, exists := rule[prevPage]
+				if exists {
+					correct = false
+					break
+				}
+			}
+		}
+
+		if !correct {
+			corrected := correctPrint(rules, print)
+
+			middle := (len(corrected) - 1) / 2
+			result += corrected[middle]
+		}
+	}
+
+	return result
+}
+
+func correctPrint(rules map[int]map[int]struct{}, print []int) []int {
+	corrected := print
+	for i := 0; i < len(corrected); i++ {
+		if i == 0 {
+			continue
+		}
+
+		rule, exists := rules[corrected[i]]
+		if !exists {
+			continue
+		}
+
+		for prevIndex, prevPage := range corrected[:i] {
+			_, exists := rule[prevPage]
+			if exists {
+				corrected[prevIndex] = corrected[i]
+				corrected[i] = prevPage
+				i = 0
+				break
+			}
+		}
+	}
+
+	return corrected
+}
